f926a81c8cd25a605bb849c310d68fb489030f0d
debugger87 <yangchaozhong.2009@gmail.com>
2015-03-23 17:49:41 +0800

    Done

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # polish-notation-calculator
 [Polish notation](http://en.wikipedia.org/wiki/Polish_notation) calculator implemented using Scala
+
+### Build
+
+```
+sbt assembly
+```
+
+### Run
+
+```
+java -cp target/scala-2.10/polish-notation-calculator-assembly-1.0.jar Calculator "/ (** 2 3) (- 4 2)"
+```
+Notes: 
+
+* `**` represents `pow`
+
+
---
+++ src/main/scala/PolishCalculator.scala
@@ -0,0 +1,49 @@
+import scala.util.parsing.combinator._
+
+trait Maths {
+  def add(x: Float, y: Float)   = x + y
+  def sub(x: Float, y: Float)   = x - y
+  def mul(x: Float, y: Float)   = x * y
+  def div(x: Float, y: Float)   = if (y > 0) (x / y) else 0.0f
+  def pow(x: Float, exp: Float) = math.pow(x, exp).toFloat
+}
+
+class PolishCalculator extends JavaTokenParsers with Maths {
+  def expr:   Parser[Float] = rep(operator ~ term) ^^ {
+    case terms =>
+      var stack  = List.empty[Float]
+      var firstOp: (Float, Float) => Float = add
+      terms.foreach(t =>
+        t match {
+          case op ~ nums => firstOp = op; stack = reduce(nums ++ stack, op)
+        }
+      )
+      stack.reduceLeft((x, y) => firstOp(y, x))
+  }
+
+  def term: Parser[List[Float]] = rep(factor)
+  def factor: Parser[Float] = num | "(" ~> expr <~ ")" ^^ (_.toFloat)
+  def num: Parser[Float] = floatingPointNumber ^^ (_.toFloat)
+  def operator: Parser[(Float, Float) => Float] = ("**" | "*" | "/" | "+" | "-") ^^ {
+    case "**" => pow
+    case "+"  => add
+    case "-"  => sub
+    case "*"  => mul
+    case "/"  => div
+  }
+
+  def reduce(nums: List[Float], op: (Float, Float) => Float): List[Float] = {
+    val result = nums.reverse match {
+      case x :: y :: xs => xs ++ List(op(y, x))
+      case List(x)      => List(x)
+      case _            => List.empty[Float]
+    }
+    result
+  }
+}
+
+object Calculator extends PolishCalculator with App{
+  def calculate(expression: String) = parseAll(expr, expression)
+
+  println(calculate(args(0)).get)
+}
